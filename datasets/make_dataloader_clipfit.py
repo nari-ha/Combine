6562a50d58b4f5e626321dacdd9451301efbd279
nari-ha <narihanar@gmail.com>
2025-04-10 15:14:28 +0100

    cross data training 업데이트

--- datasets/make_dataloader_clipfit.py
+++ datasets/make_dataloader_clipfit.py
@@ -58,87 +58,82 @@ def make_dataloader(cfg):
 
     num_workers = cfg.DATALOADER.NUM_WORKERS
 
-    if data_combine == True:
-        dataset1 = Market1501(root=cfg.DATASETS.ROOT_DIR)
-        dataset2 = MSMT17(root=cfg.DATASETS.ROOT_DIR)
-        dataset3 = VeRi(root=cfg.DATASETS.ROOT_DIR)
-        if dataset_name == "person":
-            if cfg.DEV_MODE == True:
-                dataset1 = DevMarket(root=cfg.DATASETS.ROOT_DIR)
-                dataset2 = DevMSMT(root=cfg.DATASETS.ROOT_DIR)
-            num_classes = dataset1.num_train_pids + dataset2.num_train_pids
-            cam_num = dataset1.num_train_cams + dataset2.num_train_cams
-            view_num = max(dataset1.num_train_vids, dataset2.num_train_vids)
-            train_data = dataset1.train + dataset2.train
-            query_data = dataset1.query + dataset2.query
-            gallery_data = dataset1.gallery + dataset2.gallery
-        elif dataset_name == "veri":
-            num_classes = dataset3.num_train_pids
-            cam_num = dataset3.num_train_cams
-            view_num = dataset3.num_train_vids
-            train_data = dataset3.train
-        elif dataset_name == "multi":
-            num_classes = dataset1.num_train_pids + dataset2.num_train_pids + dataset3.num_train_pids
-            cam_num = dataset1.num_train_cams + dataset2.num_train_cams + dataset3.num_train_cams
-            view_num = max(dataset1.num_train_vids, dataset2.num_train_vids, dataset3.num_train_vids)
-            train_data = dataset1.train + dataset2.train + dataset3.train
-        elif dataset_name == "market1501":
-            num_classes = dataset1.num_train_pids
-            cam_num = dataset1.num_train_cams
-            view_num = dataset1.num_train_vids
-            train_data = dataset1.train
-        elif dataset_name == "msmt17":
-            num_classes = dataset2.num_train_pids
-            cam_num = dataset2.num_train_cams
-            view_num = dataset2.num_train_vids
-            train_data = dataset2.train
-        
-        train_set = ImageDataset(train_data, train_transforms)
-        train_set_normal = ImageDataset(train_data, val_transforms)
 
-        if eval_name == 'market1501' or eval_name == 'msmt17' or eval_name == 'veri':
-            dataset = __factory[eval_name](root=cfg.DATASETS.ROOT_DIR)
-            query_data = dataset.query
-            gallery_data = dataset.gallery
-        elif eval_name == 'person':
-            query_data = dataset1.query + dataset2.query
-            gallery_data = dataset1.gallery + dataset2.gallery
-        elif eval_name == 'multi':
-            query_data = dataset1.query + dataset2.query + dataset3.query
-            gallery_data = dataset1.gallery + dataset2.gallery + dataset3.gallery
-            
-        val_set = ImageDataset(query_data + gallery_data, val_transforms)    
-        query_len = len(query_data)
-            
-        train_loader = DataLoader(
-            train_set, batch_size=cfg.SOLVER.IMS_PER_BATCH,
-            sampler=RandomIdentitySampler(train_data, cfg.SOLVER.IMS_PER_BATCH, cfg.DATALOADER.NUM_INSTANCE),
-            num_workers=num_workers, collate_fn=train_collate_fn
-        )
+    dataset1 = Market1501(root=cfg.DATASETS.ROOT_DIR)
+    dataset2 = MSMT17(root=cfg.DATASETS.ROOT_DIR)
+    dataset3 = VeRi(root=cfg.DATASETS.ROOT_DIR)
+    if dataset_name == "person":
+        if cfg.DEV_MODE == True:
+            dataset1 = DevMarket(root=cfg.DATASETS.ROOT_DIR)
+            dataset2 = DevMSMT(root=cfg.DATASETS.ROOT_DIR)
+        num_classes = dataset1.num_train_pids + dataset2.num_train_pids
+        cam_num = dataset1.num_train_cams + dataset2.num_train_cams
+        view_num = max(dataset1.num_train_vids, dataset2.num_train_vids)
+        train_data = dataset1.train + dataset2.train
+    elif dataset_name == "veri":
+        num_classes = dataset3.num_train_pids
+        cam_num = dataset3.num_train_cams
+        view_num = dataset3.num_train_vids
+        train_data = dataset3.train
+    elif dataset_name == "multi":
+        num_classes = dataset1.num_train_pids + dataset2.num_train_pids + dataset3.num_train_pids
+        cam_num = dataset1.num_train_cams + dataset2.num_train_cams + dataset3.num_train_cams
+        view_num = max(dataset1.num_train_vids, dataset2.num_train_vids, dataset3.num_train_vids)
+        train_data = dataset1.train + dataset2.train + dataset3.train
+    elif dataset_name == "market1501":
+        num_classes = dataset1.num_train_pids
+        cam_num = dataset1.num_train_cams
+        view_num = dataset1.num_train_vids
+        train_data = dataset1.train
+    elif dataset_name == "msmt17":
+        num_classes = dataset2.num_train_pids
+        cam_num = dataset2.num_train_cams
+        view_num = dataset2.num_train_vids
+        train_data = dataset2.train
+    elif dataset_name == "cross":
+        # if cfg.DEV_MODE == True:
+        num_classes = dataset2.num_train_pids + dataset3.num_train_pids
+        cam_num = dataset2.num_train_cams + dataset3.num_train_cams
+        view_num = max(dataset2.num_train_vids, dataset3.num_train_vids)
+        train_data = dataset2.train + dataset3.train
+    
+    train_set = ImageDataset(train_data, train_transforms)
+    train_set_normal = ImageDataset(train_data, val_transforms)
+    
+    if eval_name == "market1501":
+        query_data = dataset1.query
+        gallery_data = dataset1.gallery
+    elif eval_name == "msmt17":
+        query_data = dataset2.query
+        gallery_data = dataset2.gallery
+    elif eval_name == "veri":
+        query_data = dataset3.query
+        gallery_data = dataset3.gallery
+    elif eval_name == "person":
+        query_data = dataset1.query + dataset2.query
+        gallery_data = dataset1.gallery + dataset2.gallery
+    elif eval_name == "cross":
+        query_data = dataset2.query + dataset3.query
+        gallery_data = dataset2.gallery + dataset3.gallery
+    elif eval_name == "multi":
+        query_data = dataset1.query + dataset2.query + dataset3.query
+        gallery_data = dataset1.gallery + dataset2.gallery + dataset3.gallery
         
-    else:
-        print("데이터셋: ", cfg.DATASETS.NAMES)
-        dataset_train = __factory[dataset_name](root=cfg.DATASETS.ROOT_DIR)
-        train_set = ImageDataset(dataset_train.train, train_transforms)
-        train_set_normal = ImageDataset(dataset_train.train, val_transforms)
-        num_classes = dataset_train.num_train_pids
-        cam_num = dataset_train.num_train_cams
-        view_num = dataset_train.num_train_vids
-        dataset_eval = __factory[eval_name](root=cfg.DATASETS.ROOT_DIR)
-        val_set = ImageDataset(dataset_eval.query + dataset_eval.gallery, val_transforms)
-        train_loader = DataLoader(
-            train_set, batch_size=cfg.SOLVER.IMS_PER_BATCH,
-            sampler=RandomIdentitySampler(dataset_train.train, cfg.SOLVER.IMS_PER_BATCH, cfg.DATALOADER.NUM_INSTANCE),
-            num_workers=num_workers, collate_fn=train_collate_fn
-        )
-        query_len = len(dataset_eval.query)
-
+    val_set = ImageDataset(query_data + gallery_data, val_transforms)    
+        
+    train_loader = DataLoader(
+        train_set, batch_size=cfg.SOLVER.IMS_PER_BATCH,
+        sampler=RandomIdentitySampler(train_data, cfg.SOLVER.IMS_PER_BATCH, cfg.DATALOADER.NUM_INSTANCE),
+        num_workers=num_workers, collate_fn=train_collate_fn
+    )
+        
     val_loader = DataLoader(
         val_set, batch_size=cfg.TEST.IMS_PER_BATCH, shuffle=False, num_workers=num_workers,
         collate_fn=val_collate_fn
     )
+    
     train_loader_normal = DataLoader(
         train_set_normal, batch_size=cfg.TEST.IMS_PER_BATCH, shuffle=False, num_workers=num_workers,
         collate_fn=val_collate_fn
     )
-    return train_loader, train_loader_normal, val_loader, query_len, num_classes, cam_num, view_num
+    return train_loader, train_loader_normal, val_loader, len(query_data), num_classes, cam_num, view_num
